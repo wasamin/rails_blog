6122a46551de29a47a3a88cd49d05e5866e315ed
Wasamin <iwayo67@gmail.com>
2015-12-08 08:07:45 +0000

    display author name

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151129063903) do
+ActiveRecord::Schema.define(version: 20151208073357) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 20151129063903) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "name"
   end
 
   add_index "authors", ["email"], name: "index_authors_on_email", unique: true
