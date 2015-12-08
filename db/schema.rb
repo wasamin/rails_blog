343a8395249dd90642c2cf335df30bc8da1b4232
Wasamin <iwayo67@gmail.com>
2015-12-08 09:07:42 +0000

    image uplaod

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151208073357) do
+ActiveRecord::Schema.define(version: 20151208084648) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20151208073357) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "author_id"
+    t.string   "image"
   end
 
   create_table "authors", force: :cascade do |t|
